e62f82ca69f88d58cd999b234c9d2709a677d686
yun.cai <yun.cai@amlogic.com>
2013-11-06 14:40:59 +0800

    Revert "scripts/Kconfig:add isource support."
    This reverts commit 26f03f86bba941b38fc8c2cba976498735ded66b.

--- scripts/kconfig/lkc.h
+++ scripts/kconfig/lkc.h
@@ -33,7 +33,6 @@ extern "C" {
 
 #define LOCALEDIR "/usr/share/locale"
 
-
 #define _(text) gettext(text)
 #define N_(text) (text)
 
@@ -123,7 +122,6 @@ void menu_set_type(int type);
 
 /* util.c */
 struct file *file_lookup(const char *name);
-int file_exist(const char *name);
 int file_write_dep(const char *name);
 void *xmalloc(size_t size);
 void *xcalloc(size_t nmemb, size_t size);
