c2f543622e2670bac72a0d4f3806207f53590b74
brian.zhu <brian.zhu@amlogic.com>
2013-11-13 02:44:37 +0800

    remove no cached flag for res buffer interface, this flag will be set at the user of v4l2 driver mmap

--- drivers/media/v4l2-core/videobuf-res.c
+++ drivers/media/v4l2-core/videobuf-res.c
@@ -247,7 +247,7 @@ static int __videobuf_mmap_mapper(struct videobuf_queue *q,
 	size = (size < mem->size) ? size : mem->size;
 
 	//vma->vm_page_prot = vm_get_page_prot(vma->vm_flags);
-	vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
+	//vma->vm_page_prot = pgprot_noncached(vma->vm_page_prot);
 	retval = remap_pfn_range(vma, vma->vm_start,
 				 mem->phy_addr >> PAGE_SHIFT,
 				 size, vma->vm_page_prot);
