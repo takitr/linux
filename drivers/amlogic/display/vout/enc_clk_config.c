be8250a0e6a05ab3e0e974465caccd796ea29ab5
tao.dong <tao.dong@amlogic.com>
2013-11-28 16:44:19 +0800

    Revert "hdmitx: optimise hdmi phy setting"
    This reverts commit 0199f7ad535bec77718faf3543d92fabe57627f3.

--- drivers/amlogic/display/vout/enc_clk_config.c
+++ drivers/amlogic/display/vout/enc_clk_config.c
@@ -32,8 +32,6 @@ static void set_hpll_clk_out(unsigned clk)
     aml_write_reg32(P_HHI_VID_PLL_CNTL3, 0xca563823);
     aml_write_reg32(P_HHI_VID_PLL_CNTL4, 0x00238100);
     aml_write_reg32(P_HHI_VID_PLL_CNTL5, 0x00012286);
-    aml_write_reg32(P_HHI_HDMI_PHY_CNTL1, 2);
-    aml_write_reg32(P_HHI_HDMI_PHY_CNTL0, 0x08930e9b);
 #endif
     switch(clk){
 #ifdef CONFIG_ARCH_MESON8
@@ -50,7 +48,7 @@ static void set_hpll_clk_out(unsigned clk)
                 ;
             }
             aml_write_reg32(P_HHI_HDMI_PHY_CNTL1, 2);
-            aml_write_reg32(P_HHI_HDMI_PHY_CNTL0, 0x08930d5b);
+            aml_write_reg32(P_HHI_HDMI_PHY_CNTL0, 0x08c34d0b);
             break;
 #endif
         case 1488:
@@ -63,7 +61,7 @@ static void set_hpll_clk_out(unsigned clk)
     }
     aml_write_reg32(P_HHI_VID_PLL_CNTL2, 0x69c8ce00);
     aml_write_reg32(P_HHI_HDMI_PHY_CNTL1, 2);
-    aml_write_reg32(P_HHI_HDMI_PHY_CNTL0, 0x08930e9b);
+    aml_write_reg32(P_HHI_HDMI_PHY_CNTL0, 0x08c38d0b);
 #endif
 #ifdef CONFIG_ARCH_MESON6
             WRITE_CBUS_REG(HHI_VID_PLL_CNTL, 0x43e);
