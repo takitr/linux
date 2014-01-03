2b79eb8b394a23b934ecd96f4b367bcbf15302f6
timyao <tim.yao@amlogic.com>
2014-01-03 00:20:30 -0800

    m6tvd:video: add ucode of m6tvd
    Enable video, decoders, deinterlacer, RDMA, PPMGR, ION drivers for M6TVD platform
    
    
    Former-commit-id: da7ea5f52f8ae51925e76f49913e528e1e5547e5

--- drivers/amlogic/amports/vmpeg12.c
+++ drivers/amlogic/amports/vmpeg12.c
@@ -639,7 +639,7 @@ static void vmpeg12_prot_init(void)
 
     WRITE_VREG(POWER_CTL_VLD, save_reg);
 
-#elif MESON_CPU_TYPE == MESON_CPU_TYPE_MESON6
+#elif (MESON_CPU_TYPE == MESON_CPU_TYPE_MESON6) || (MESON_CPU_TYPE == MESON_CPU_TYPE_MESON6TVD)
     WRITE_VREG(DOS_SW_RESET0, (1<<7) | (1<<6));
     WRITE_VREG(DOS_SW_RESET0, 0);
 #else
