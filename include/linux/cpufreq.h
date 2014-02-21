d763248245d96d15517e97a6b7a9762e37985f3a
shi.liu <shi.liu@amlogic.com>
2014-02-21 16:44:00 +0800

    PD #87439: cpufreq: add sys node : scaling_dflt_freq headfile
    Former-commit-id: 9cd828e51bfa7eeb0bc88358130cebd3d060226a

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -84,6 +84,7 @@ struct cpufreq_cpuinfo {
 struct cpufreq_real_policy {
 	unsigned int		min;    /* in kHz */
 	unsigned int		max;    /* in kHz */
+	unsigned int		dflt;    /* in kHz */
 	unsigned int		policy; /* see above */
 	struct cpufreq_governor	*governor; /* see below */
 };
@@ -102,6 +103,7 @@ struct cpufreq_policy {
 
 	unsigned int		min;    /* in kHz */
 	unsigned int		max;    /* in kHz */
+	unsigned int		dflt;    /* in kHz */
 	unsigned int		cur;    /* in kHz, only needed if cpufreq
 					 * governors are used */
 	unsigned int		policy; /* see above */
