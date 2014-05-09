9e960cd647cddac12c0cba7f7868abd6bb2d9633
Jerry Cao <jerry.cao@amlogic.com>
2014-05-09 14:53:12 -0700

    Remove non-Amlogic DVB tuner/demod driver code.
    These 3rd party driver does not belong to Amlogic kernel tree, thus delete
    them from the source tree.
    
    
    Former-commit-id: 731c019cae62d4e737f6a08edbc9ef91ce0dec44

--- drivers/amlogic/dvb_tv/dibcom8096/i2c.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADAPTER_I2C_H
-#define ADAPTER_I2C_H
-
-#include "databus.h"
-
-#endif
--- drivers/amlogic/dvb_tv/ite9173/version.h
+++
@@ -1,3 +0,0 @@
-#define Version_NUMBER		0x0203
-#define Version_DATE		0x20120529
-#define Version_BUILD		0x00
