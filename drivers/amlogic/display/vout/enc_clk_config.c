8f0d1b6994f66e02656ddc16f075227e39a2c45b
zongdong.jiao <zongdong.jiao@amlogic.com>
2014-05-27 20:34:30 +0800

    PD #92355: hdmitx: hpll: optimize hpll at 1.485GHz
    Former-commit-id: 35a7a7fe8198e5e9f86f1308a69ee62c889d01d2

--- drivers/amlogic/display/vout/enc_clk_config.c
+++ drivers/amlogic/display/vout/enc_clk_config.c
@@ -76,18 +76,14 @@ static void set_hpll_clk_out(unsigned clk)
         case 1488:
 #ifdef CONFIG_ARCH_MESON8B
             aml_write_reg32(P_HHI_VID_PLL_CNTL2, 0x69c8ce00);
-            aml_write_reg32(P_HHI_VID_PLL_CNTL4, 0x40238100);
-            aml_write_reg32(P_HHI_VID_PLL_CNTL5, 0x12385);
+            aml_write_reg32(P_HHI_VID_PLL_CNTL4, 0x4023d100);
+            aml_write_reg32(P_HHI_VID_PLL_CNTL3, 0x8a7ad023);
+            aml_write_reg32(P_HHI_VID_PLL_CNTL5, 0x12286);
             aml_write_reg32(P_HHI_VID_PLL_CNTL,  0x6000043d);
             aml_write_reg32(P_HHI_VID_PLL_CNTL,  0x4000043d);
             while(!(aml_read_reg32(P_HHI_VID_PLL_CNTL) & (1 << 31))) {
                 ;
             }
-            msleep(1);
-            aml_write_reg32(P_HHI_VID_PLL_CNTL2, 0x69c80e00);
-            aml_write_reg32(P_HHI_VID_PLL_CNTL3, 0x0e79c822);
-            aml_write_reg32(P_HHI_VID_PLL_CNTL,  0x400121ef);
-            break;
 #endif
 #ifdef CONFIG_ARCH_MESON8
             aml_write_reg32(P_HHI_VID_PLL_CNTL,  0x6000043d);
