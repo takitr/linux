3641adb0ad9b7ab8c689e0b4ccae33dbbcaeae29
kele.bai <kele.bai@amlogic.com>
2014-07-05 10:05:37 +0800

    [M8M2]:update di version defination.
    Former-commit-id: 64a88897ac71a525ea1ad328f1d79c2cbe0dd3c1

--- drivers/amlogic/deinterlace/deinterlace.h
+++ drivers/amlogic/deinterlace/deinterlace.h
@@ -32,7 +32,7 @@
 #define NEW_DI_TV
 #define NEW_DI_V1 //from m6tvc
 #define NEW_DI_V2 //from m6tvd(noise meter bug fix,improvement for 2:2 pull down)
-#elif (MESON_CPU_TYPE==MESON_CPU_TYPE_MESON8 || MESON_CPU_TYPE==MESON_CPU_TYPE_MESON8B)
+#elif (MESON_CPU_TYPE==MESON_CPU_TYPE_MESON8 || MESON_CPU_TYPE==MESON_CPU_TYPE_MESON8B || MESON_CPU_TYPE == MESON_CPU_TYPE_MESON8M2)
 #define NEW_DI_V1 //from m6tvc
 #define NEW_DI_V2 //from m6tvd(noise meter bug fix,improvement for 2:2 pull down)
 #endif
