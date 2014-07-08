d1bca96fab7a4be4ffc49cfc482302a61eddf638
kele.bai <kele.bai@amlogic.com>
2014-07-08 10:24:25 +0800

    [M8M2]:add m6 di version null.
    Former-commit-id: 07eb231e39de394d02d40c2e3ce87c423fe45d82

--- drivers/amlogic/deinterlace/deinterlace.h
+++ drivers/amlogic/deinterlace/deinterlace.h
@@ -35,7 +35,7 @@
 #elif (MESON_CPU_TYPE==MESON_CPU_TYPE_MESON8 || MESON_CPU_TYPE==MESON_CPU_TYPE_MESON8B || MESON_CPU_TYPE == MESON_CPU_TYPE_MESON8M2)
 #define NEW_DI_V1 //from m6tvc
 #define NEW_DI_V2 //from m6tvd(noise meter bug fix,improvement for 2:2 pull down)
-#else
+#elif (MESON_CPU_TYPE > MESON_CPU_TYPE_MESON8M2)
 #define NEW_DI_V1 //from m6tvc
 #define NEW_DI_V2 //from m6tvd(noise meter bug fix,improvement for 2:2 pull down)
 #endif
