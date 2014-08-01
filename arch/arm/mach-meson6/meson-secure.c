2b2ece8cf31adccdd587ee4126b728b3a0350068
xueyue.ma <xueyue.ma@amlogic.com>
2014-08-01 16:20:14 +0800

    PD#95290: TrustZone HAL APIs using cache method
    Conflicts:
    
    	arch/arm/mach-meson6/meson-secure.c
    
    
    Former-commit-id: e9b886e2a278c9e7a0ea83452c78a2d6cbd19a8a

--- arch/arm/mach-meson6/meson-secure.c
+++ arch/arm/mach-meson6/meson-secure.c
@@ -64,7 +64,7 @@ int meson_secure_memblock(unsigned startaddr, unsigned endaddr, struct secure_me
 }
 */
 
-struct memconfig memsecure[MEMCONFIG_NUM];
+struct memconfig memsecure[MEMCONFIG_NUM] = {};
 int meson_trustzone_memconfig(void)
 {
 	int ret;
@@ -73,17 +73,11 @@ int meson_trustzone_memconfig(void)
 	arg.memconfigbuf_phy_addr = __pa(memsecure);
 	arg.memconfigbuf_count = MEMCONFIG_NUM;
 
-	__cpuc_flush_dcache_area(memsecure, sizeof(memsecure));
-	outer_clean_range(__pa(memsecure), (__pa(memsecure + MEMCONFIG_NUM)));
-	__cpuc_flush_dcache_area(&arg, sizeof(arg));
-	outer_clean_range(__pa(&arg), __pa(((struct memconfig_hal_api_arg*)&arg)) + 1);
+	set_cpus_allowed_ptr(current, cpumask_of(0));
 
 	ret = meson_smc_hal_api(TRUSTZONE_HAL_API_MEMCONFIG, __pa(&arg));
 
-	outer_inv_range(__pa(&arg), __pa(((struct memconfig_hal_api_arg*)&arg)) + 1);
-	dmac_unmap_area(&arg, sizeof(arg), DMA_FROM_DEVICE);
-	outer_inv_range(__pa(memsecure), __pa(memsecure + MEMCONFIG_NUM));
-	dmac_unmap_area(memsecure, sizeof(memsecure), DMA_FROM_DEVICE);
+	set_cpus_allowed_ptr(current, cpu_all_mask);
 
 	return ret;
 }
@@ -127,23 +121,9 @@ int meson_trustzone_efuse(struct efuse_hal_api_arg* arg)
 		return -1;
 	}
 	set_cpus_allowed_ptr(current, cpumask_of(0));
-	__cpuc_flush_dcache_area(__va(arg->buffer_phy), arg->size);
-	outer_clean_range((arg->buffer_phy), (arg->buffer_phy + arg->size));
-
-	__cpuc_flush_dcache_area(__va(arg->retcnt_phy), sizeof(unsigned int));
-	outer_clean_range(arg->retcnt_phy, (arg->retcnt_phy + sizeof(unsigned int)));
-
-	__cpuc_flush_dcache_area((void*)arg, sizeof(struct efuse_hal_api_arg));
-	outer_clean_range(__pa(arg), __pa(arg + 1));
 
 	ret = meson_smc_hal_api(TRUSTZONE_HAL_API_EFUSE, __pa(arg));
 
-	if (arg->cmd == EFUSE_HAL_API_READ) {
-		outer_inv_range((arg->buffer_phy), (arg->buffer_phy + arg->size));
-		dmac_unmap_area(__va(arg->buffer_phy), arg->size, DMA_FROM_DEVICE);
-	}
-	outer_inv_range((arg->retcnt_phy), (arg->retcnt_phy + sizeof(unsigned int)));
-	dmac_unmap_area(__va(arg->buffer_phy), arg->size, DMA_FROM_DEVICE);
 	set_cpus_allowed_ptr(current, cpu_all_mask);
 
 	return ret;
